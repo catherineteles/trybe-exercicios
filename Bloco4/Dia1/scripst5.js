d560c4daa206d9b177291ad927318939fe83d157
Catherine Teles <cacateles12@gmail.com>
2021-11-26 16:35:21 -0300

    exercicio 5 corrigido

--- Bloco4/Dia1/scripst5.js
+++ Bloco4/Dia1/scripst5.js
@@ -2,8 +2,11 @@ let a = 30
 let b = 60
 let c = 90
 
-if (a + b + c == 180 && a>0 && b>0 &&c>0){
+if (a + b + c == 180){
     console.log(true);
+}
+if else(a<0 || b<0 || c<0){
+    console.log("Erro!Ângulo Inválido");
 }
 else{
     console.log(false);
