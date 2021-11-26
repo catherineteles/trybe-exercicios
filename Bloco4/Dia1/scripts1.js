7cb4b2354eea0e697ad299496e6d0cfbe382cc65
Catherine Teles <cacateles12@gmail.com>
2021-11-26 16:10:48 -0300

    Exercicios 1-3

---
+++ Bloco4/Dia1/scripts1.js
@@ -0,0 +1,8 @@
+let a = 2
+let b = 8
+
+console.log(a+b);
+console.log(a-b);
+console.log(a*b);
+console.log(a/b);
+console.log(a%b);
---
+++ Bloco4/Dia1/scripts2.js
@@ -0,0 +1,12 @@
+let a = 3
+let b = 7
+
+if (a > b){
+    console.log(a);
+}
+else if (a < b){
+    console.log(b);
+}
+else{
+    console.log("Os números são iguais");
+}
