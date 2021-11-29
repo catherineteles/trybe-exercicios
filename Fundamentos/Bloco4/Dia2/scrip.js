ede1d38eee5afd55f67df54580bb7d970cc2e264
Catherine Teles <cacateles12@gmail.com>
2021-11-29 15:54:48 -0300

    exercicio1

---
+++ Fundamentos/Bloco4/Dia2/scrip.js
@@ -0,0 +1,9 @@
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+
+//Percorra o array imprimindo todos os números no console.log()
+
+    for (let number of numbers){
+        console.log(number);
+    }
+
+
