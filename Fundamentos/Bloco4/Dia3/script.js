d07a76ab0b9468e8ab7a4b1a136c03e970361a57
Catherine Teles <cacateles12@gmail.com>
2021-12-01 09:25:52 -0300

    exercicio 1 sem ser do bonus

--- Fundamentos/Bloco4/Dia3/script.js
+++ Fundamentos/Bloco4/Dia3/script.js
@@ -1,22 +1,10 @@
-//Depois, faça uma pirâmide com n asteriscos de base:
+//O fatorial é a multiplicação de um número natural pelos seus antecessores, exceto o zero. Com base nessas informações, crie um algoritmo que retorne o fatorial de 10.
 
-let n = 5;
-let meio = (n + 1) / 2;
-let esquerda = meio;
-let direita = meio;
-let linha = "";
+let n = 10;
+let resultadoMultiplicacao = n;
 
-for (let l = 0; l <= meio; l++) {
-    for (let c = 0; c <=  n; c++){
-        if (c > esquerda && c < direita){
-            linha = linha + "*";
-        } else {
-            linha = linha + " ";
-        }
-    }
-    console.log(linha);
-        linha = "";
-        direita += 1;
-        esquerda -= 1;
+for (let fatorial = 1; fatorial < n; fatorial++){
+resultadoMultiplicacao = resultadoMultiplicacao*fatorial;
 }
 
+console.log(resultadoMultiplicacao);
