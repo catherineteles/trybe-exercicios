ef335fc84e682c7601744ca3f7530eb93fc30201
Catherine Teles <cacateles12@gmail.com>
2021-12-01 11:34:06 -0300

    Exercicio1_3

---
+++ Fundamentos/Bloco4/Dia3/script1_3.js
@@ -0,0 +1,27 @@
+//Considere o array de strings abaixo:
+let array = ['java', 'javascript', 'python', 'html', 'css'];
+
+//Escreva dois algoritmos: um que retorne a maior palavra deste array e outro que retorne a menor. Considere o número de caracteres de cada palavra.
+
+let maiorPalavra = array[0];
+
+for (index = 0; index < array.length; index++){
+    
+        if(array[index].length > maiorPalavra.length){
+            maiorPalavra = array[index];
+        }
+        
+}
+
+console.log(maiorPalavra);
+
+let menorPalavra = array[0]
+
+for (index = 0; index < array.length; index++){
+    
+    if(array[index].length < menorPalavra.length){
+        menorPalavra = array[index];
+    }
+    
+}
+console.log(menorPalavra);
