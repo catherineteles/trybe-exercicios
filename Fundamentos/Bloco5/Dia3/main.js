12e507e4e028aa7e1f12dc247c513e7c0bd5c0a4
Catherine Teles <cacateles12@gmail.com>
2021-12-07 19:17:11 -0300

    Função changeTech

--- Fundamentos/Bloco5/Dia3/main.js
+++ Fundamentos/Bloco5/Dia3/main.js
@@ -12,7 +12,16 @@ const myWebpage = document.getElementById('my-spotrybefy');
 
 // 2. Crie uma função que adicione a classe 'tech' ao elemento `li` quando for clicado.
 // 2.1. Deve existir apenas um elemento com a classe 'tech'. Como você faz isso?
+function changeTech(event) {
+  const techElement = document.querySelector('.tech');
+  techElement.classList.remove('tech');
+  event.target.classList.add('tech');
+  input.value ='';
+}
 
+firstLi.addEventListener('click', changeTech);
+secondLi.addEventListener('click', changeTech);
+thirdLi.addEventListener('click', changeTech);
 
 // 3. Crie uma função que, ao digitar na caixa de texto, altere o texto do elemento
 // com a classe 'tech';
