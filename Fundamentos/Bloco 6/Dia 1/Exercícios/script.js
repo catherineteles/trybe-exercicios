5914bc487165bb800d05977d74ac2f3996a5f2b4
Catherine Teles <cacateles12@gmail.com>
2022-01-05 18:17:22 -0300

    formulario

---
+++ Fundamentos/Bloco 6/Dia 1/Exercícios/script.js
@@ -0,0 +1,8 @@
+const stateSelect = document.getElementById('state');
+let statesList = ["AC", "AL", "AM", "AP", "BA", "CE", "DF", "ES", "GO", "MA", "MT", "MS", "MG", "PA", "PB", "PR", "PE", "PI", "RJ", "RN", "RO", "RS", "RR", "SC", "SE", "SP", "TO"];
+
+for(let index = 0; index < statesList.length; index +=1){
+    let states = document.createElement('option');
+    states.innerText = statesList[index];
+    stateSelect.appendChild(states);
+}
