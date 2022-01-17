569326410b7f463070454ed2f7bb86eac89a4ddd
Catherine Teles <cacateles12@gmail.com>
2022-01-16 22:15:45 -0300

    trocando test por it

--- Fundamentos/Bloco 7/Dia3/sum.test.js
+++ Fundamentos/Bloco 7/Dia3/sum.test.js
@@ -1,21 +1,21 @@
 const sum = require('./sum');
 
-test('adds 4 + 5 to equal 9', () => {
+it('adds 4 + 5 to equal 9', () => {
     expect(sum(4,5)).toBe(9);
 });
 
-test('adds 0 + 0 to equal 0', () => {
+it('adds 0 + 0 to equal 0', () => {
     expect(sum(0,0)).toBe(0);
 });
 
-test('adds string throw error', () => {
+it('adds string throw error', () => {
     function addString() {
       sum(4,'5');
     };
     expect(addString).toThrowError(Error);
 });
 
-test('adds string throw error: parameters must be numbers', () => {
+it('adds string throw error: parameters must be numbers', () => {
     function addString() {
       sum(4,'5');
     };
