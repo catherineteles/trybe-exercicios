d3ab0f08369cbf2a41079837e7d746a8334fbcf5
Catherine Teles <cacateles12@gmail.com>
2022-01-18 19:15:27 -0300

    exercicios do dia

---
+++ Fundamentos/Bloco8/dia1-introduction-to-hof/hofs.js
@@ -0,0 +1,40 @@
+//Crie uma função que retorne um objeto no formato { nomeCompleto, email } representando uma nova pessoa contratada.
+// Passe sua função como parâmetro da HOF newEmployees para criar cada pessoa contratada em seu respectivo id. 
+//A sua função deve receber como parâmetro o nome completo da pessoa funcionária e a partir dele gerar automaticamente
+// um email no formato nome_da_pessoa@trybe.com .
+
+const emailInformation = (fullname) => {
+    //https://www.w3schools.com/jsref/jsref_join.asp
+    const email = fullname.split(' ').join('_');
+    return {fullname, email: `${email.toLowerCase()}@trybe.com` }
+}
+const newEmployees = (callback) => {
+    const employees = {
+      id1: callback('Pedro Guerra'), // Nome: Pedro Guerra -> Chame sua função passando o nome Pedro Guerra como parâmetro, substituindo as aspas
+      id2: callback('Luiza Drumond'), // Nome: Luiza Drumond -> Chame sua função passando o nome Luiza Drumond como parâmetro, substituindo as aspas
+      id3: callback('Carla Paiva'), // Nome: Carla Paiva -> Chame sua função passando o nome Carla Paiva como parâmetro, substituindo as aspas
+    }
+    return employees;
+  };
+
+  console.log(newEmployees(emailInformation));
+
+  //Desenvolva uma HOF que retorna o resultado de um sorteio. 
+  //Esta HOF irá gerar um número aleatório entre 1 e 5 recebendo como parâmetros o número apostado
+  // e uma função que checa se o número apostado é igual ao número sorteado. 
+  //O retorno da sua HOF deve ser uma string (Ex: "Tente novamente" ou "Parabéns você ganhou").
+
+const numberCheck = (num1,num2) => {
+    if(num1 === num2){
+        return true;
+    }
+}  
+const lottery = (num, callback) => {
+    const lotteryNumber = Math.round(Math.random() * 5);
+    if( callback(num,lotteryNumber)){
+        return "Parabéns você ganhou"
+    }
+    return `Tente novamente, o numero sorteado foi ${lotteryNumber}`
+};
+
+console.log(lottery(2, numberCheck));
---
+++ Fundamentos/Bloco8/dia1-introduction-to-hof/hofs2.js
@@ -0,0 +1,24 @@
+//Crie uma HOF que receberá três parâmetros. O primeiro será um array de respostas corretas (Gabarito), 
+//o segundo será um array de respostas a serem verificadas (respostas da pessoa estudante) 
+//e o terceiro é uma função que checa se as respostas estão corretas e faz a contagem da pontuação final 
+//recebida pela pessoa estudante. Ao final a HOF deve retornar o total da contagem de respostas certas.
+
+
+const RIGHT_ANSWERS = ['A', 'C', 'B', 'D', 'A', 'A', 'D', 'A', 'D', 'C'];
+const STUDENT_ANSWERS = ['A', 'N.A', 'B', 'D', 'A', 'C', 'N.A', 'A', 'D', 'B'];
+
+const checkAnswers = (gabarito, student) => {
+    let contador = 0;
+    for(let index = 0; index < gabarito.length; index += 1){
+        if(gabarito[index] === student[index]){
+            contador += 1;
+        } else if (student[index] === 'N.A'){
+            contador += 0;
+        } else {
+            contador -= 0.5;
+        }
+    }
+    return contador
+}
+
+console.log(checkAnswers(RIGHT_ANSWERS, STUDENT_ANSWERS));
