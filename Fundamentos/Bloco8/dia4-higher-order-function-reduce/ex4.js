2d51844d80f7eb7b6a204ddbdbf0f27e41b1c761
Catherine Teles <cacateles12@gmail.com>
2022-01-23 19:05:24 -0300

    exercicio 4

--- Fundamentos/Bloco8/dia4-higher-order-function-reduce/ex4.js
+++ Fundamentos/Bloco8/dia4-higher-order-function-reduce/ex4.js
@@ -61,4 +61,27 @@ const books = [
     },
   ];
   
-  // Adicione o código do exercício aqui:
+  // Encontre o livro com o maior nome.
+
+
+const expectedResult = {
+    id: 1,
+    name: 'As Crônicas de Gelo e Fogo',
+    genre: 'Fantasia',
+    author: {
+      name: 'George R. R. Martin',
+      birthYear: 1948,
+    },
+    releaseYear: 1991,
+  };
+  
+  function longestNamedBook(array) {
+    return array.reduce((acc, current) => {
+        if(acc.name.length < current.name.length){
+            acc = current;
+        }
+        return acc;
+    });
+  }
+
+  console.log(longestNamedBook(books));
