8f9540586cdba415a8d36e306346935f87bdd6f0
Catherine Teles <cacateles12@gmail.com>
2022-02-01 11:38:04 -0300

    exercicio 2

---
+++ Fundamentos/Bloco9/dia-2-fetch-api-async-await/api.js
@@ -0,0 +1,24 @@
+const append = (listItem) => {
+    const ul = document.getElementById('coin-list');
+    const li = document.createElement('li');
+    li.innerHTML = listItem;
+    ul.appendChild(li);
+}
+
+const extractInfo = (array) => {
+    return `${array.id} (${array.symbol}): valor em dólares $${array.priceUsd} `;
+}
+
+
+async function getCrypto() {
+    try {
+      const response = await fetch('https://api.coincap.io/v2/assets');
+      const data = await response.json();
+      const item = data.data.map(extractInfo);
+      item.forEach(append);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  getCrypto();
