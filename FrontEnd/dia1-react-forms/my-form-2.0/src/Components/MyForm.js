17e1b67835c83ccba1e2ae8d4162b73834521541
cathe <cacateles12@gmail.com>
2022-02-22 18:30:37 -0300

    Added MyForm and PersonalData components

---
+++ FrontEnd/dia1-react-forms/my-form-2.0/src/Components/MyForm.js
@@ -0,0 +1,15 @@
+import React from "react";
+import PersonalData from "./PersonalData";
+
+class MyForm extends React.Component {
+    render(){
+    return <div>
+            <h1>Meu Formulário 2.0</h1>
+            <form>
+                <PersonalData />
+            </form>
+        </div>
+    }
+}
+
+export default MyForm;
---
+++ FrontEnd/dia1-react-forms/my-form-2.0/src/Components/PersonalData.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+class PersonalData extends React.Component {
+    render(){}
+}
+
+export default PersonalData;
